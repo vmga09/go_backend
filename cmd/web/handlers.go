f8e9bd86b5ebb2e959373df47fbe5e093cccf5b8
vmga09 <victor.gonzalez.a@gmail.com>
2025-08-20 23:16:04 -0400

    tarea s5 terminada

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -27,14 +27,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
